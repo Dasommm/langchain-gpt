31b4ac85e32783cc843d1ba317a5e104939bcaff
dasommm <dasom.kwon92@gmail.com>
2025-12-29 17:17:00 +0900

    05 MeeingGPT : video의 내용을 summary

---
+++ pages/05_MeetingGPT.py
@@ -0,0 +1,190 @@
+import streamlit as st
+import subprocess
+import math
+from pydub import AudioSegment
+import glob
+import openai
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import StrOutputParser
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+
+llm = ChatOpenAI(
+    temperature= 1,
+)
+
+## 이미 이 파일이 있는 경우 재실행하지 않게 하기 위해 생성
+has_transcript = os.path.exists("./.cache/JENNIE_interview.txt")
+
+## 한개의 document를 여러개의 documents로 나눈다.
+## ChatModel에서 이 documents를 순회하면서 더 나은 결과를 만들어낸다(=refine)
+splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=800,
+    chunk_overlap=100,
+)
+
+@st.cache_data()
+def embed_file(file_path):
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=800,
+        chunk_overlap=100,
+    )
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+## STT : whisper 사용
+@st.cache_data()
+def transcribe_chunks(chunk_folder, destination):
+    if has_transcript:
+        return
+    files = glob.glob(f"{chunk_folder}/*.mp3")
+    files.sort()
+    for file in files:
+        with open(file, "rb") as audio_file, open(destination, "a") as text_file:
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            text_file.write(transcript.text)
+
+## video를 audio로 변경하는 함수
+@st.cache_data()
+def extract_audio_from_video(video_path):
+    if has_transcript:
+        return
+    audio_path = video_path.replace("mp4", "mp3")
+    command = ["ffmpeg", "-y", "-i", video_path, "-vn", audio_path]
+    subprocess.run(command)
+    
+    
+## audio 파일을 10분마다 잘라주는 함수
+@st.cache_data()  
+def cut_audio_in_chunks(audio_path, chunk_size, chunks_folder):
+    if has_transcript:
+        return
+    track = AudioSegment.from_mp3("./files/audio.mp3")
+    # track.duration_seconds
+    # ten_minutes = 10 * 60 * 1000 ## 밀리초 기준
+    chunk_len = chunk_size * 60 *1000
+    
+    chunks = math.ceil(len(track) / chunk_len)
+    for i in range(chunks):
+        start_time = i * chunk_len
+        end_time = (i+1) * chunk_len
+        
+        chunk = track[start_time:end_time]
+        chunk.export(f"{chunks_folder}/chunk_{i}.mp3", format="mp3")
+
+st.set_page_config(
+    page_title="MeetingGPT",
+    page_icon="💼",
+)
+
+st.markdown(
+    """
+# MeetingGPT
+            
+Welcome to MeetingGPT, upload a video and I will give you a transcript, a summary and a chat bot to ask any questions about it.
+
+Get started by uploading a video file in the sidebar.
+"""
+)
+
+with st.sidebar:
+    video = st.file_uploader(
+        "Video",
+        type=["mp4", "avi", "mkv", "mov"],
+    )
+    
+if video:
+    with st.status("Loading video...") as status:
+        chunks_folder = "./.cache/chunks"
+        video_content = video.read()
+        video_path = f"./.cache/{video.name}"
+        audio_path = video_path.replace("mp4", "mp3")
+        transcript_path = video_path.replace("mp4", "txt")
+        with open(video_path, "wb") as f:
+            f.write(video_content)
+        status.update(label="Extracting audio...")
+        extract_audio_from_video(video_path)
+        status.update(label="Cutting audio segments...")
+        cut_audio_in_chunks(audio_path, 10, chunks_folder)
+        status.update(label="Transcribing audio...")
+        transcribe_chunks(chunks_folder, transcript_path)
+        
+    transcript_tab, summary_tab, qa_tab = st.tabs(
+        [
+            "Transcript",
+            "Summary",
+            "Q&A",
+        ]
+    )
+        
+    with transcript_tab:
+        with open(transcript_path, "r") as file:
+            st.write(file.read())
+
+    with summary_tab:
+        start = st.button("Generate summary")
+        if start:
+            loader = TextLoader(transcript_path)
+            docs = loader.load_and_split(text_splitter=splitter)
+            
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """
+                Write a concise summary of the following:
+                "{text}"
+                CONCISE SUMMARY:                
+            """
+            )
+            
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+
+            summary = first_summary_chain.invoke(
+                {"text": docs[0].page_content},
+            )
+            
+            ## documents를 돌면서 더 나은 결과를 생성한다.
+            refine_prompt = ChatPromptTemplate.from_template(
+                """
+                Your job is to produce a final summary.
+                We have provided an existing summary up to a certain point: {existing_summary}
+                We have the opportunity to refine the existing summary (only if needed) with some more context below.
+                ------------
+                {context}
+                ------------
+                Given the new context, refine the original summary.
+                If the context isn't useful, RETURN the original summary.
+                """
+            )
+
+            refine_chain = refine_prompt | llm | StrOutputParser()
+            
+            with st.status("Summarizing...") as status:
+                for i, doc in enumerate(docs[1:]):  ## 0번은 위에서 시작했기 때문에 그 두번째인 1번부터 시작한다. 
+                    status.update(label=f"Processing document {i+1}/{len(docs)-1} ")
+                    summary = refine_chain.invoke(
+                        {
+                            "existing_summary": summary,
+                            "context": doc.page_content,
+                        }
+                    )
+                    st.write(summary)
+            st.write(summary)
+            
+                
+    with qa_tab:
+        ## TODO: 파일에 대해서 질문과 답을 할 수 있도록 수정
+        retriever = embed_file(transcript_path)
+        docs = retriever.invoke("do they talk about blackpink?")
+        st.write(docs)
+
+   
